f14ee055c0130a4377b8ad511bffb27bb4a9e9a1
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-05-16 00:26:17 +0900

    岡からのコメントを追加しました

--- favoriteFood/src/favoriteFood/Main.java
+++ favoriteFood/src/favoriteFood/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("好きな食べ物はクレープです");
 		System.out.println("その理由は甘いものが好きだからです");
+		
+		System.out.println("クレープ美味しいですよね。大学の近くにもお店があればなあ...　by岡");
 	}
 }
